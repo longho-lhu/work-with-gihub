31f949ef25edd3d4e7daef374c9213ac933eda2c
Hoàng Long <longhnh@lhu.edu.vn>
2024-11-25 09:06:01 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function test() {
   console.log("Hello")
+  console.log('line 2')
+  console.log('line 3')
   const var1 = 123;
   const var2 = "abc";
 
-    console.log('line 2')
-    console.log('line 3')
+
+  function subFunc(a, b, c) {
+    return (a + b + c) / 3;
+  }
 }
